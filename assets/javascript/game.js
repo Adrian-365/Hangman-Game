7aec0b805b20bb48997e7756d4df70e74429e31c
Adrian Nalwalker <analwalker@planetfg.local>
2017-10-08 08:27:44 -0700

    framing

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//id's: qA graphic remaining guessed winLoss
+
+// guesses remaining = 10
+// wins and losses
+
+// If guesses remaining = 0 then you lose.
+
+// questions and answers, arranged as objects with the answer being an array.  
+// Blanks as an array as well.
+
+// listen for key click
+// 	if ??? is empty then initialize
+// 	if ??? is not empty then check key against answer
+// 		if key = one of the answers then display that character
+// 		if key != one of the answers then decrease quesses remaining
+// 	if guesses remaining = 0 then prompt to start again.
+
+
+
+// initialize function to pick q1, then q2, then q3 etc.
+
